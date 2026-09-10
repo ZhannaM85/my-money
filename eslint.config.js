b37353fb1f0d7c341c215650763fdb442b576036
Zhanna M <zhanna.myshkovskaya@gmail.com>
2026-09-10 20:10:37 +0300

    #226: ignore .d.ts files in ESLint so Pages deploy passes

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint'
 import eslintConfigPrettier from 'eslint-config-prettier'
 
 export default tseslint.config(
-  { ignores: ['dist', 'android', 'ios'] },
+  { ignores: ['dist', 'android', 'ios', '**/*.d.ts'] },
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
